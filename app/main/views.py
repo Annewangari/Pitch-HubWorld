0bbe98719975e672e0ffa7e5972d7064bfafdd23
Annewangari <annekare3@gmail.com>
2020-10-23 11:41:45 +0300

    create update profile route

---
+++ app/main/views.py
@@ -0,0 +1,43 @@
+from flask import Flask, flash, redirect, render_template, request, session, abort, url_for
+from . import main 
+from flask_login import login_required
+from .forms import ReviewForm, UpdateProfile
+
+@main.route('/')
+def home():
+    if not session.get('logged_in'):
+        return render_template('login.html')
+    else:
+        return "Hello Boss!"
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username=uname).first()
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user=user)
+
+
+@main.route('/user/<uname>/update', methods=['GET', 'POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username=uname).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile', uname=user.username))
+
+    return render_template('profile/update.html', form=form)
+
+@main.route('/pitch/review/new/<int:id>', methods=['GET', 'POST'])
+@login_required
+def new_review(id):
+    pass
